7ac86dd0e4582e1f52094538cf56327ef5587cbb
RishabJasiwal <lionelmess04012002@gmail.com>
2025-05-11 18:26:12 +0530

    form validtions added

--- src/main/java/in/ashokit/binding/Product.java
+++ src/main/java/in/ashokit/binding/Product.java
@@ -1,9 +1,17 @@
 package in.ashokit.binding;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Product {
 	
+	@NotNull(message="Id is required")
 	private Integer pid;
+	@NotNull(message="Name is required")
+	@Size(min = 3 ,max = 8 )
 	private String pname;
+	@NotNull(message="Price is required")
+	
 	private Double price;
 	public Integer getPid() {
 		return pid;
--- src/main/java/in/ashokit/controller/ProductController.java
+++ src/main/java/in/ashokit/controller/ProductController.java
@@ -2,10 +2,12 @@ package in.ashokit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import in.ashokit.binding.Product;
+import jakarta.validation.Valid;
 
 @Controller
 public class ProductController {
@@ -18,9 +20,15 @@ public class ProductController {
 	
 	//method to handle form submission
 	@PostMapping("/product")
-	public String handleSubmitBtn(Product p,Model model) {
+	public String handleSubmitBtn(@Valid Product p, BindingResult result,Model model) {
 		System.out.println(p);
+		
+		if(result.hasErrors()) {
+			return "index";
+		}else {
 		model.addAttribute("msg", "Product Saved");
+		}
 		return "index";
+		
 	}
 }
